f298303be33dce3c714556254253b6f2b28aed4e
hardik sharma <118838121+hardiksharmaa@users.noreply.github.com>
2025-04-18 23:04:58 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (76.78%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -1,30 +1,24 @@
 class Solution {
 public:
+    int solve(vector<int>& nums){
+        int prev=0;
+        int prev1=nums[0];
+        for(int i=1;i<nums.size();i++){
+            int inc=nums[i]+prev;
+            int exc=prev1;
+            int ans=max(inc,exc);
+            prev=prev1;
+            prev1=ans;
+        }
+        return prev1;
+    }
     int rob(vector<int>& nums) {
         if(nums.size()==1) return nums[0];
-
-        vector<int>skiphFirst(nums.begin()+1,nums.end());
-        vector<int>skiphLast(nums.begin(),nums.end()-1);
-        for(int i=0;i<nums.size()-1;i++){
-            skiphFirst[i]=nums[i+1];
-            skiphLast[i]=nums[i];
+        vector<int> first,last;
+        for(int i=0;i<nums.size();i++){
+            if(i!=nums.size()-1) first.push_back(nums[i]);
+            if(i!=0) last.push_back(nums[i]);
         }
-
-        int loot1=helper(skiphFirst);
-        int loot2=helper(skiphLast);
-
-        return max(loot1,loot2);
-    }
-
-    int helper(vector<int> &nums){
-        if(nums.size()==1) return nums[0];
-        vector<int>dp(nums.size());
-        dp[0]=nums[0];
-        dp[1]=max(nums[0],nums[1]);
-
-        for(int i=2;i<nums.size();i++){
-            dp[i]=max(dp[i-2]+nums[i],dp[i-1]);
-        }
-        return dp[nums.size()-1];
+        return max(solve(first),solve(last));
     }
 };
