b83eb3f8bb1068689221f09c5febc3bf87a4d11b
hardik sharma <118838121+hardiksharmaa@users.noreply.github.com>
2025-04-22 00:56:59 +0530

    Time: 29 ms (45.32%) | Memory: 16.3 MB (77.27%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,27 +1,23 @@
 class Solution {
 public:
-    int m, n;  // Class-level variables to store grid dimensions
-
-    void DFS(vector<vector<char>>& grid, int i, int j) {
-        if (i < 0 || i >= m || j < 0 || j >= n || grid[i][j] != '1') return;
-
-        grid[i][j] = '0';  // Mark the cell as visited
-        DFS(grid, i + 1, j);
-        DFS(grid, i - 1, j);
-        DFS(grid, i, j + 1);
-        DFS(grid, i, j - 1);
+    int n,m;
+    void DFS(vector<vector<char>>& grid,int i,int j){
+        if(i<0||i>=n||j<0||j>=m || grid[i][j]=='0') return;
+        grid[i][j]='0';
+        DFS(grid,i+1,j);
+        DFS(grid,i-1,j);
+        DFS(grid,i,j+1);
+        DFS(grid,i,j-1);
     }
-
     int numIslands(vector<vector<char>>& grid) {
-        m = grid.size();
-        n = grid[0].size();
-        int count = 0;
-
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (grid[i][j] == '1') {  // Found an unvisited part of an island
-                    DFS(grid, i, j);
-                    count++;  // Increase the island count
+        int count=0;
+        n=grid.size();
+        m=grid[0].size();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]=='1'){
+                    DFS(grid,i,j);
+                    count++;
                 }
             }
         }
