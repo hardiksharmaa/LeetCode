32f5330581217b867cc0e63884236f1b99084da1
hardik sharma <118838121+hardiksharmaa@users.noreply.github.com>
2025-04-26 20:07:14 +0530

    Time: 0 ms (100.00%) | Memory: 29.3 MB (57.06%) - LeetSync

--- 3034-points-that-intersect-with-cars/points-that-intersect-with-cars.cpp
+++ 3034-points-that-intersect-with-cars/points-that-intersect-with-cars.cpp
@@ -1,12 +1,28 @@
+// class Solution {
+// public:
+//     int numberOfPoints(vector<vector<int>>& nums) {
+//         set<int> covered;
+//         for(auto& num: nums){
+//             for(int i=num[0];i<=num[1];i++){
+//                 covered.insert(i);
+//             }
+//         }
+//         return covered.size();
+//     }
+// };
 class Solution {
 public:
     int numberOfPoints(vector<vector<int>>& nums) {
-        set<int> covered;
+        vector<int> covered(101);
         for(auto& num: nums){
             for(int i=num[0];i<=num[1];i++){
-                covered.insert(i);
+                covered[i]=1;
             }
         }
-        return covered.size();
+        int count=0;
+        for(int i=0;i<covered.size();i++){
+            if(covered[i]==1) count++;
+        }
+        return count;
     }
 };
