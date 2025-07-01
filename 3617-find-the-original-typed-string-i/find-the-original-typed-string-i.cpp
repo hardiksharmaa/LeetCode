f0272592390beeef274148ebaf7bf7a372faeecf
hardik sharma <118838121+hardiksharmaa@users.noreply.github.com>
2025-07-01 21:36:21 +0530

    Time: 2 ms (48.46%) | Memory: 8.9 MB (60.22%) - LeetSync

---
+++ 3617-find-the-original-typed-string-i/find-the-original-typed-string-i.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int possibleStringCount(string word) {
+        int count=1;
+        int maxi=1;
+        for(int i=1;i<word.length();i++){
+            if(word[i]==word[i-1]){
+                count++;
+                if(count>maxi) maxi=count;
+            }
+        }
+        return maxi;
+    }
+};
