35dfc913d2a469f4a837260f8bd079be0dcdcc26
hardik sharma <118838121+hardiksharmaa@users.noreply.github.com>
2025-07-27 21:36:14 +0530

    Time: 0 ms (100.00%) | Memory: 40.2 MB (57.83%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -5,7 +5,7 @@ public:
         vector<int> ans(n,1);
         int prefix=1;
         for(int i=0;i<n;i++){
-            ans[i]=prefix;
+            ans[i]*=prefix;
             prefix*=nums[i];
         }
         int suffix=1;
@@ -13,6 +13,7 @@ public:
             ans[i]*=suffix;
             suffix*=nums[i];
         }
+
         return ans;
     }
 };
