1b7e2354a8bfc461d08c1e7be3a5aa01829baaa7
hardik sharma <118838121+hardiksharmaa@users.noreply.github.com>
2025-07-27 18:39:22 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (74.21%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,11 +1,16 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
         if(s.length()!=t.length()) return false;
-        for(int i=0;i<t.length();i++){
-            if(t[i]!=s[i]){
+        int vis[26]={0};
+
+        for(int i=0;i<s.size();i++){
+            vis[s[i]-'a']++;
+            vis[t[i]-'a']--;
+        }
+
+        for(int i=0;i<26;i++){
+            if(vis[i]!=0){
                 return false;
                 break;
             }
