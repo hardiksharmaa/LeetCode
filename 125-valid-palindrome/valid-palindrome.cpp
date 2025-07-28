8d32ba6017244c087c9b5bf120ff45c132158475
hardik sharma <118838121+hardiksharmaa@users.noreply.github.com>
2025-07-28 22:34:00 +0530

    Time: 1 ms (48.37%) | Memory: 10.2 MB (28.37%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -1,20 +1,17 @@
 class Solution {
 public:
-    string solve(const string& s){
-        string newstr;
-        for(char c:s){
-            if(isalnum(c)){
-                newstr+=tolower(c);
-            }
-        }
-        return newstr;
-    }
     bool isPalindrome(string s) {
-        string ans=solve(s);
+        string str;
+        for(char c: s){
+            if(isalnum(c) && c!=' ') str+=tolower(c);
+        }
         int i=0;
-        int j=ans.length()-1;
-        while(i<j){
-            if(ans[i]!=ans[j]) return false;
+        int j=str.size()-1;
+        while(i<=j){
+            if(str[i]!=str[j]){
+                return false;
+                break;
+            }
             else{
                 i++;
                 j--;
