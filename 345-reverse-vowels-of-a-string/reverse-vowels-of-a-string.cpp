ce87e2b5e346a9f06e8a225938a71e595f928f0c
hardik sharma <118838121+hardiksharmaa@users.noreply.github.com>
2025-08-13 22:05:19 +0530

    Time: 0 ms (100.00%) | Memory: 10.3 MB (80.40%) - LeetSync

--- 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.cpp
+++ 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.cpp
@@ -3,18 +3,17 @@ public:
     string reverseVowels(string s) {
         int i=0;
         int j=s.length()-1;
+        string vowels="aeiouAEIOU";
         while(i<j){
-            if((s[i]=='a'||s[i]=='A'||s[i]=='e'||s[i]=='E'||s[i]=='i'||s[i]=='I'||s[i]=='o'||s[i]=='O'||s[i]=='u'||s[i]=='U') &&(s[j]=='a'||s[j]=='A'||s[j]=='e'||s[j]=='E'||s[j]=='i'||s[j]=='I'||s[j]=='o'||s[j]=='O'||s[j]=='u'||s[j]=='U')){
-                swap(s[i],s[j]);
-                i++;
-                j--;
-            }
-            else if(s[i]!='a'&&s[i]!='A'&&s[i]!='e'&&s[i]!='E'&&s[i]!='i'&&s[i]!='I'&&s[i]!='o'&&s[i]!='O'&&s[i]!='u'&&s[i]!='U'){
+            while(i<j && vowels.find(s[i])==string::npos){
                 i++;
             }
-            else if(s[j]!='a'&&s[j]!='A'&&s[j]!='e'&&s[j]!='E'&&s[j]!='i'&&s[j]!='I'&&s[j]!='o'&&s[j]!='O'&&s[j]!='u'&&s[j]!='U'){
+            while(i<j && vowels.find(s[j])==string::npos){
                 j--;
             }
+            swap(s[i],s[j]);
+            i++;
+            j--;
         }
         return s;
     }
