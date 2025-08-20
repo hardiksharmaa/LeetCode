eac92e93bd311394b9a4eb2015ac54af6c4bf9ff
hardik sharma <118838121+hardiksharmaa@users.noreply.github.com>
2025-08-20 14:59:19 +0530

    Time: 1021 ms (41.39%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1773-percentage-of-users-attended-a-contest/percentage-of-users-attended-a-contest.sql
@@ -0,0 +1,5 @@
+SELECT r.contest_id,
+       ROUND(COUNT(DISTINCT r.user_id) * 100.0 / (SELECT COUNT(*) FROM Users), 2) AS percentage
+FROM Register r
+GROUP BY contest_id
+order by percentage desc, contest_id;
