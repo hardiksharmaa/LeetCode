1dc5a6c3ebe8b83dcc5779b5b7ae2fe73d07471e
hardik sharma <118838121+hardiksharmaa@users.noreply.github.com>
2025-08-26 20:24:38 +0530

    Time: 52 ms (64.04%) | Memory: 55.2 MB (24.55%) - LeetSync

--- 2741-function-composition/function-composition.js
+++ 2741-function-composition/function-composition.js
@@ -3,14 +3,13 @@
  * @return {Function}
  */
 var compose = function(functions) {
-    if(functions.length===0){
-        return function(x) {return x;};
+    return function(x){
+        let ans=x;
+        for(let i=functions.length-1;i>=0;i--){
+            ans=functions[i](ans);
+        }
+        return ans;
     }
-    return functions.reduceRight(function(prev,next){
-        return function(x){
-            return next(prev(x));
-        };
-    });
 };
 
 /**
