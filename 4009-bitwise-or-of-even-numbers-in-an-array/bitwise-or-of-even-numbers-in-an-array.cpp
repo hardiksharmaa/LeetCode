7b772224e5901f36af86ad69f45810e38bbd8193
hardik sharma <118838121+hardiksharmaa@users.noreply.github.com>
2025-09-25 00:32:01 +0530

    Time: 0 ms (100.00%) | Memory: 24.3 MB (99.89%) - LeetSync

--- 4009-bitwise-or-of-even-numbers-in-an-array/bitwise-or-of-even-numbers-in-an-array.cpp
+++ 4009-bitwise-or-of-even-numbers-in-an-array/bitwise-or-of-even-numbers-in-an-array.cpp
@@ -3,15 +3,15 @@ public:
     int evenNumberBitwiseORs(vector<int>& nums) {
         int ans=0;
         int j=0;
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]%2==0){
-                ans=nums[i];
-                j=i;
-                break;
-            }
-        }
+        // for(int i=0;i<nums.size();i++){
+        //     if(nums[i]%2==0){
+        //         ans=nums[i];
+        //         j=i;
+        //         break;
+        //     }
+        // }
 
-        for(int i=j+1;i<nums.size();i++){
+        for(int i=0;i<nums.size();i++){
             if(nums[i]%2==0){
                 ans|=nums[i];
             }
