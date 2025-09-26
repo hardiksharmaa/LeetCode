21d9ce0e66e2cfa3a3f0e2908121bac79334ced0
hardik sharma <118838121+hardiksharmaa@users.noreply.github.com>
2025-09-26 19:21:38 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (7.79%) - LeetSync

---
+++ 3636-check-balanced-string/check-balanced-string.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isBalanced(string num) {
+        int evensum=0;
+        int oddsum=0;
+        for(int i=0;i<num.length();i++){
+            if(i%2==0){
+                evensum+=num[i]-'0';
+            }
+            else oddsum+=num[i]-'0';
+        }
+        if(evensum==oddsum) return true;
+        else return false;
+    }
+};
