4f2ce3bc1ebfbe9eb75bda5333f67c6a457d375f
hardik sharma <118838121+hardiksharmaa@users.noreply.github.com>
2025-09-29 17:00:11 +0530

    Time: 3 ms (55.56%) | Memory: 10.5 MB (33.60%) - LeetSync

---
+++ 4053-majority-frequency-characters/majority-frequency-characters.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    string majorityFrequencyGroup(string s) {
+        unordered_map<char,int> mp;
+        for(char c:s){
+            mp[c]++;
+        }
+        int maxi=0;
+        string ans="";
+        int maxFreq = 0;
+        for (auto& [key, value] : mp) {
+            maxFreq = max(maxFreq, value);
+        }
+        vector<vector<char>> bucket(maxFreq + 1);
+        for(auto& [key,value]:mp){
+            bucket[value].push_back(key);
+        }
+        int n=bucket.size();
+        for(int i=n-1;i>=0;i--){
+            if(bucket[i].size()>maxi){
+                maxi=bucket[i].size();
+                ans.clear();
+                for(char c:bucket[i]){
+                    ans+=c;
+                }
+            }
+        }
+        return ans;
+    }
+};
