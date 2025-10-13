5e61740c7dbbe3051af6b9340c70ed933f88d50d
hardik sharma <118838121+hardiksharmaa@users.noreply.github.com>
2025-10-13 15:23:44 +0530

    Time: 14 ms (7.80%) | Memory: 20.6 MB (8.53%) - LeetSync

--- 1353-find-resultant-array-after-removing-anagrams/find-resultant-array-after-removing-anagrams.cpp
+++ 1353-find-resultant-array-after-removing-anagrams/find-resultant-array-after-removing-anagrams.cpp
@@ -1,20 +1,43 @@
 class Solution {
 public:
     vector<string> removeAnagrams(vector<string>& words) {
-        vector<string> ans;//abba
-        string x=words[0];//aabb
-        ans.push_back(x);
-        sort(x.begin(),x.end());
+        // vector<string> ans;//abba
+        // string x=words[0];//aabb
+        // ans.push_back(x);
+        // sort(x.begin(),x.end());
+        // for(int i=1;i<words.size();i++){
+        //     string y=words[i];
+        //     sort(words[i].begin(),words[i].end());//aabb
+        //     if(x==words[i]){
+        //         continue;
+        //     }
+        //     else{
+        //         x=words[i];
+        //         ans.push_back(y);
+
+        //     }
+        // }
+        // return ans;
+
+        vector<string> ans;
+        unordered_map<char,int> prev;
+        for(char c:words[0]){
+            prev[c]++;
+        }
+        string x=words[0];
+        ans.push_back(x);//abba
         for(int i=1;i<words.size();i++){
             string y=words[i];
-            sort(words[i].begin(),words[i].end());//aabb
-            if(x==words[i]){
+            unordered_map<char ,int> curr;
+            for(char c: words[i]){
+                curr[c]++;
+            }
+            if(curr==prev){
                 continue;
             }
             else{
-                x=words[i];
+                prev=curr;
                 ans.push_back(y);
-
             }
         }
         return ans;
