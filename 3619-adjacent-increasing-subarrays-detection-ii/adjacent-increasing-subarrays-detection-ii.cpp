44d3f79f9af73156f7534b79c9384e917b919710
hardik sharma <118838121+hardiksharmaa@users.noreply.github.com>
2025-10-15 11:42:29 +0530

    Time: 241 ms (30.83%) | Memory: 182.4 MB (24.44%) - LeetSync

--- 3619-adjacent-increasing-subarrays-detection-ii/adjacent-increasing-subarrays-detection-ii.cpp
+++ 3619-adjacent-increasing-subarrays-detection-ii/adjacent-increasing-subarrays-detection-ii.cpp
@@ -1,21 +1,47 @@
 class Solution {
 public:
     int maxIncreasingSubarrays(vector<int>& nums) {
+        // int n=nums.size();
+        // vector<int> end(n,1),start(n,1);
+        // for(int i=1;i<n;i++){
+        //     if(nums[i]>nums[i-1]){
+        //         end[i]=end[i-1]+1;
+        //     }
+        // }
+        // for(int i=n-2;i>=0;i--){
+        //     if(nums[i]<nums[i+1]){
+        //         start[i]=start[i+1]+1;
+        //     }
+        // }
+        // int ans=1;
+        // for(int k=0;k+1<n;k++){
+        //     ans=max(ans,min(end[k],start[k+1]));
+        // }
+        // return ans;//O(n)
+
+
         int n=nums.size();
-        vector<int> end(n,1),start(n,1);
-        for(int i=1;i<n;i++){
-            if(nums[i]>nums[i-1]){
-                end[i]=end[i-1]+1;
-            }
-        }
+        if(n<2) return 0;
+        vector<int> inc(n,1);
         for(int i=n-2;i>=0;i--){
             if(nums[i]<nums[i+1]){
-                start[i]=start[i+1]+1;
+                inc[i]=inc[i+1]+1;
             }
         }
-        int ans=1;
-        for(int k=0;k+1<n;k++){
-            ans=max(ans,min(end[k],start[k+1]));
+        auto func=[&](int k){
+            if(k==0) return true;
+            for(int i=0;i+2*k<=n;i++){
+                if(inc[i]>=k && inc[i+k]>=k){
+                    return true;
+                }
+            }
+            return false;
+        };
+        int low=0,high=n/2,ans=0;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(func(mid)) ans=mid,low=mid+1;
+            else high=mid-1;
         }
         return ans;
     }
