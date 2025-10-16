bbe3e687fbce5ba3bb7c627e64201fd99855c5ef
hardik sharma <118838121+hardiksharmaa@users.noreply.github.com>
2025-10-16 23:33:49 +0530

    Time: 76 ms (37.22%) | Memory: 135.8 MB (34.36%) - LeetSync

---
+++ 2661-smallest-missing-non-negative-integer-after-operations/smallest-missing-non-negative-integer-after-operations.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int findSmallestInteger(vector<int>& nums, int value) {
+        unordered_map<int, int> freq;
+        for (int x : nums) {
+            int r=((x%value)+value)%value;
+            freq[r]++;
+        }
+        int mex = 0;
+        while (true) {
+            int r = mex % value;
+            if (freq[r] == 0) break;
+            freq[r]--;
+            mex++;
+        }
+        return mex;
+    }
+};
