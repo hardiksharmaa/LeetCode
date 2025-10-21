f8767de9ecdbfcaa1ce2c27d0da33a443644c0c8
hardik sharma <118838121+hardiksharmaa@users.noreply.github.com>
2025-10-22 00:44:26 +0530

    Time: 7 ms (98.54%) | Memory: 96.3 MB (81.55%) - LeetSync

--- 3622-maximum-frequency-of-an-element-after-performing-operations-i/maximum-frequency-of-an-element-after-performing-operations-i.cpp
+++ 3622-maximum-frequency-of-an-element-after-performing-operations-i/maximum-frequency-of-an-element-after-performing-operations-i.cpp
@@ -1,20 +1,29 @@
 class Solution {
 public:
-    static int maxFrequency(vector<int>& nums, int k, int numOperations) {
-        static constexpr int N=1e5+2;
-        int freq[N]={0}, sweep[N]={0}, mm=N, MM=0;
-        for(int x: nums){
+    int maxFrequency(vector<int>& nums, int k, int numOperations) {
+        int maxi=*max_element(nums.begin(),nums.end())+k;
+        vector<int> freq(maxi+1,0);
+
+        for(int &x:nums){
             freq[x]++;
-            const int x0=max(1, x-k), xN=min(x+k+1, N-1);
-            sweep[x0]++;
-            sweep[xN]--;
-            mm=min(mm, x0);
-            MM=max(MM, xN);
         }
-        int ans=0, cnt=0;
-        for (int x=mm; x<=MM; x++){
-            cnt+=sweep[x];
-            ans=max(ans, freq[x]+min(cnt-freq[x], numOperations));
+
+        for(int i=1;i<=maxi;i++){
+            freq[i]+=freq[i-1];
+        }
+        int ans=0;
+
+        for(int i=0;i<=maxi;i++){
+            if(freq[i]==0) continue;
+
+            int l=max(0,i-k);
+            int r=min(maxi,i+k);
+
+            int totalcount=freq[r]-(l>0?freq[l-1]:0);
+            int target=freq[i]-(i>0?freq[i-1]:0);
+            int needed=totalcount-target;
+            int maxpossible=target+min(numOperations,needed);
+            ans=max(ans,maxpossible);
         }
         return ans;
     }
