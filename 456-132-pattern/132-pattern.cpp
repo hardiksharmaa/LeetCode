eeb13ac5a87d340c60cda0e6d0e61d69442a6e57
hardik sharma <118838121+hardiksharmaa@users.noreply.github.com>
2025-10-29 13:49:21 +0530

    Time: 12 ms (49.95%) | Memory: 70.6 MB (61.72%) - LeetSync

---
+++ 456-132-pattern/132-pattern.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool find132pattern(vector<int>& nums) {
+        int n=nums.size();
+        if(n<3) return false;
+        stack<int> st;
+        int middle=INT_MIN;
+        for(int i=n-1;i>=0;i--){
+            if(nums[i]<middle){
+                return true;
+            }
+            while(!st.empty() && st.top()<nums[i]){
+                middle=st.top();
+                st.pop();
+            }
+            st.push(nums[i]);
+        }
+        return false;
+    }
+};
