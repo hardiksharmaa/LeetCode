97e5495276fe50511df4b23c1ab1f6b5aa33229e
hardik sharma <118838121+hardiksharmaa@users.noreply.github.com>
2026-02-16 21:23:27 +0530

    Time: 4 ms (29.28%) | Memory: 8.2 MB (73.18%) - LeetSync

--- 190-reverse-bits/reverse-bits.cpp
+++ 190-reverse-bits/reverse-bits.cpp
@@ -4,8 +4,8 @@ public:
         uint32_t ans = 0;
         for (int i = 0; i < 32; i++) {
             ans <<= 1;
-            ans |= (n & 1);
-            n >>= 1;
+            ans |= (n & 1);//appending last bit form n to the result
+            n >>= 1;// removing last bit form n
         }
         return ans;
     }
