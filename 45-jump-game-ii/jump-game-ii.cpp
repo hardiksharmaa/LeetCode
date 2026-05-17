49af05fb32685dfa9f624ebe404296626e124d26
hardik sharma <118838121+hardiksharmaa@users.noreply.github.com>
2026-05-17 18:35:31 +0530

    Time: 0 ms (100.00%) | Memory: 20.3 MB (82.90%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cpp
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -1,19 +1,22 @@
 class Solution {
 public:
     int jump(vector<int>& nums) {
-        int jumps=0;
-        int lastind=0;
-        int cover=0;
-        if(nums.size()<=1) return 0;
-        for(int i=0;i<nums.size();i++){
-            if(i>cover) return false;
-            cover=max(cover,i+nums[i]);
-            if(i==lastind){
-                lastind=cover;
-                jumps++;
-            }
-            if(lastind>=nums.size()-1) break;
+        int covered=0;
+        int currend=0;
+        int n=nums.size();
+        int steps=0;
+        if(n==1){
+            return 0;
         }
-        return jumps;
+        for(int i=0;i<n-1;i++){
+            if(covered>=i){
+                covered=max(covered,i+nums[i]);
+            }
+            if(i==currend){
+                steps++;
+                currend=covered;
+            }
+        }
+        return steps;
     }
 };
