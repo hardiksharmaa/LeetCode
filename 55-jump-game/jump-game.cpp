ecdb961f009f6931269bbbed47e810f0a9ee8e5a
hardik sharma <118838121+hardiksharmaa@users.noreply.github.com>
2026-05-17 17:32:16 +0530

    Time: 0 ms (100.00%) | Memory: 52.2 MB (81.51%) - LeetSync

--- 55-jump-game/jump-game.cpp
+++ 55-jump-game/jump-game.cpp
@@ -2,13 +2,23 @@ class Solution {
 public:
     bool canJump(vector<int>& nums) {
         int n=nums.size();
-        int coverage=0;
-        for(int i=0;i<n;i++){
-            if(i>coverage) return false;
-            coverage=max(coverage,i+nums[i]);
-            if(coverage>=n-1) return true;
+        int covered=0;
+        if(n==1){
+            return true;
+        }
+        else{
+            if(nums[0]==0){
+                return false;
+            }
+        }
+        for(int i=0;i<n-1;i++){
+            if(covered>=i){
+                covered=max(covered, i+nums[i]);
+            }
+            if(covered>=n-1){
+                return true;
+            }
         }
         return false;
-
     }
 };
